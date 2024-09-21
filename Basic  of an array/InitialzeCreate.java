20c6c3898a4d7babb736f5eb968efc6668db415b
nitesh-kumar09 <papamumma990@gmail.com>
2024-09-22 01:42:19 +0530

    Change in the file 'InitialzeCreate.java' add new concept in the code that is length propertie or identifier in java

--- Basic  of an array/InitialzeCreate.java
+++ Basic  of an array/InitialzeCreate.java
@@ -9,12 +9,16 @@
         int[] even = {2,4,6,8};
 
         // Print the array.
-
+        System.out.println("*******The aray is*******");
         System.out.println(even[0]);
         System.out.println(even[1]);
         System.out.println(even[2]);
         System.out.println(even[3]);
 
+
+        // find the length of an above array.
+        System.out.println("*******Length of an aray is*******");
+        System.out.println(even.length);
         
         
     }

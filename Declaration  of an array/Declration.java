c246695bd92dcf6298c5e38e0b9ac3f27187f537
nitesh-kumar09 <papamumma990@gmail.com>
2024-09-22 01:05:30 +0530

    I create a folder 'Declaration of an array' and iside it i just create a file name 'Declation.java' and write how to declare array in java.

---
+++ Declaration  of an array/Declration.java
@@ -0,0 +1,23 @@
+class Declration
+{
+    public static void main(String[] args)
+    {
+        // Declaration of an 1-Diemsional array.
+        int[] arr;
+        int []arr1;
+        int arr2[];
+
+         // Declaration of an 2-Diemsional array.
+        int[][] arr3;
+        int [][]arr4;
+        int arr5[][];
+        int[] []arr6;
+        int[] arr7[];
+        int []arr8[];
+
+        // we can also do samething for 3-Dimensional array.
+        int[][][] arr9;
+
+       
+    }
+}

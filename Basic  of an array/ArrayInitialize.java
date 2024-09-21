20103854bb1067a30b266038182a45520eb85eab
nitesh-kumar09 <papamumma990@gmail.com>
2024-09-22 01:50:52 +0530

    create a file 'ArrayInitialize.java' and write code to store value in fruits array on by one in index and also print using for each loop.

---
+++ Basic  of an array/ArrayInitialize.java
@@ -0,0 +1,19 @@
+
+ class ArrayInitialize {
+    
+    public static void main(String[] args)
+    {
+        String[] fruits = new String[4];
+        fruits[0] = "Apple";
+        fruits[1] = "Orange";
+        fruits[2] = "Banana";
+        fruits[3] = "Pineapple";
+
+        for(String items:fruits)
+        {
+            System.out.println(items);
+        }
+
+    }
+
+}

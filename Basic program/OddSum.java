bec0c94f573829031c7e71a52d546d12cde51d4d
nitesh-kumar09 <papamumma990@gmail.com>
2024-09-22 03:52:15 +0530

    Writting program to calculate sum of odd number in the file 'OddSum.java'

---
+++ Basic program/OddSum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class OddSum {
+
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        int size = obj.nextInt();
+
+        int oddSum = 0 , i = 0;
+
+        int[] odd = new int[size];
+
+        System.out.println("Enter Element in the array : ");
+        while(i<odd.length)
+        {
+            odd[i] = obj.nextInt();
+            i++;
+        }
+
+        for(int item:odd)
+        {
+            if(item%2!=0){
+                oddSum += item;
+            }
+        }
+        System.out.println("The sum is :  "+oddSum);
+    }
+    
+}

ebe1dbe7de3ec9107313d160fa8224eb1f07063e
nitesh-kumar09 <papamumma990@gmail.com>
2024-09-22 01:36:51 +0530

    create a file that is 'InitialzeCreate' and write the code of creation , initialization of an arrays in one line.

---
+++ Basic  of an array/InitialzeCreate.java
@@ -0,0 +1,21 @@
+
+
+ class InitialzeCreate {
+    
+
+    public static void main(String[] args)
+    {
+        // create initialize in one line.
+        int[] even = {2,4,6,8};
+
+        // Print the array.
+
+        System.out.println(even[0]);
+        System.out.println(even[1]);
+        System.out.println(even[2]);
+        System.out.println(even[3]);
+
+        
+        
+    }
+}

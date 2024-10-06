75afcf8aa90d432853c09d995c81e21bc050dca0
nitesh-kumar09 <papamumma990@gmail.com>
2024-10-07 00:45:06 +0530

    Create file that is 'ThirdMinMax.java' in folder Sorting and write logic.

---
+++ Sorting/ThirdMinMax.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class ThirdMinMax {
+
+
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("Enter size of an array : ");
+        int size = obj.nextInt();
+
+        System.out.println("Enter elements of an array : ");
+
+        int[] sort = new int[size];
+
+        for(int i = 0 ; i < sort.length ; i++)
+        {
+            sort[i] = obj.nextInt();
+        }
+
+        Arrays.sort(sort);
+
+       System.out.println("Second smallest number is : " +sort[2]);
+
+       System.out.println("Second larget number is : " + sort[sort.length-3]);
+
+
+
+    }
+    
+}

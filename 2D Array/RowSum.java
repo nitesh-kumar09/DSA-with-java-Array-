e7a9be4d0bf99c1204c69aaba5f03e6c3d8819b8
nitesh-kumar09 <papamumma990@gmail.com>
2024-10-22 20:08:40 +0530

    i just implement the logic to sum of row only

---
+++ 2D Array/RowSum.java
@@ -0,0 +1,43 @@
+// program to read and calculate sum of all the elements present in the matrix.
+
+import java.util.*;
+
+
+
+class RowSum {
+
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter size of row : ");
+        int rSize = obj.nextInt();
+
+        System.out.println("Enter size of column : ");
+        int cSize = obj.nextInt();
+
+        int[][] a = new int[rSize][cSize];
+
+        System.out.println("Enter element of the matrix : ");
+        for(int i = 0 ; i < rSize ; i++)
+        {
+            for(int j = 0 ; j < cSize ; j++)
+            {
+                a[i][j] = obj.nextInt();
+            }
+        }
+
+        
+        for(int i = 0 ; i <rSize ; i++)
+        {
+            int sum = 0 ;
+            for(int j = 0 ; j < cSize ; j++)
+            {
+                sum = sum+a[i][j];
+            }
+            System.out.println("The sum of all the element present "+ i +" row is : "  +sum);
+        }
+
+        
+    }
+    
+}
